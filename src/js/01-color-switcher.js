69c1256bac5eb81544d1adfb7051599c9946d4a3
Denys <k.denis.aa@gmail.com>
2022-11-26 17:15:57 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,39 @@
+const refs = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+console.log(refs);
+
+refs.startBtn.disabled = false;
+refs.stopBtn.disabled = true;
+
+let timerId;
+
+function onStartClick() {
+  timerId = setInterval(changeBodyColor, 1000);
+  toggleButtonsState();
+}
+
+function onStopClick() {
+  clearInterval(timerId);
+  toggleButtonsState();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
+
+function toggleButtonsState() {
+  refs.startBtn.disabled = !refs.startBtn.disabled;
+  refs.stopBtn.disabled = !refs.stopBtn.disabled;
+}
+
+//
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);

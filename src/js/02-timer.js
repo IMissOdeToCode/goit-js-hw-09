bbc48c1eca17b8ae12c0699f039aaa7f2706c47f
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-27 23:07:51 +0200

    add flatpickr

---
+++ src/js/02-timer.js
@@ -0,0 +1,26 @@
+// Описаний в документації
+import flatpickr from 'flatpickr';
+// Додатковий імпорт стилів
+import 'flatpickr/dist/flatpickr.min.css';
+
+const refs = {
+  datetime_picker: document.querySelector('#datetime-picker'),
+
+  getRefs() {
+    console.log(({ datetime_picker } = this));
+  },
+};
+
+refs.getRefs();
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+
+flatpickr(refs.datetime_picker, { options });
